a24028dbe906700d743b53637f960d1991c7cd9b
Evelin Ponce <evelin@kinedu.com>
2020-07-06 21:56:53 -0500

    Add form template

---
+++ app/public/js/scripts.js
@@ -0,0 +1,298 @@
+/* Template: Cedo - Loans Credit Landing Page Template
+   Author: Inovatik
+   Created: Nov 2019
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+	
+	/* Navbar Scripts */
+	// jQuery to collapse the navbar on scroll
+	$(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+	});
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+    // closes the responsive menu on menu item click
+    $(".navbar-nav li a").on("click", function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+    /* Image Slider - Swiper */
+    var imageSlider = new Swiper('.image-slider', {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+		},
+        loop: false,
+        navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
+    });
+
+
+	/* Card Slider - Swiper */
+	var cardSlider = new Swiper('.card-slider', {
+		autoplay: {
+			delay: 4000,
+		},
+        loop: false,
+        pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+		slidesPerView: 3,
+		spaceBetween: 0,
+        breakpoints: {
+            992: {
+                slidesPerView: 1
+            }
+        }
+	});
+
+
+    /* Details Lightbox - Magnific Popup */
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+
+
+	/* Image Lightbox - Magnific Popup */
+	$('.popup-link').magnificPopup({
+		removalDelay: 300,
+		type: 'image',
+		callbacks: {
+			beforeOpen: function() {
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure ' + this.st.el.attr('data-effect'));
+			},
+			beforeClose: function() {
+				$('.mfp-figure').addClass('fadeOut');
+			}
+		},
+		gallery:{
+			enabled:true //enable gallery mode
+		}
+    });
+    
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+    });
+
+
+	/* Get Quote Form */
+    $("#getEbookForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            gformError();
+            gsubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            gsubmitForm();
+        }
+    });
+
+    function gsubmitForm() {
+        // initiate variables with form content
+		var firstname = $("#gfirstname").val();
+		var lastname = $("#glastname").val();
+        var phone = $("#gphone").val();
+		var email = $("#gemail").val();
+		var company = $("#gcompany").val();
+        var select = $("#gselect").val();
+        var data_string =  "firstname=" + firstname + "lastname=" + lastname + "&phone=" + phone + "&email=" + email + "company=" + company + "&select=" + select 
+
+        console.log(data_string);
+        gformSuccess();
+        // $.ajax({
+        //     type: "POST",
+        //     url: "php/getquoteform-process.php",
+        //     data: "name=" + name + "&phone=" + phone + "&email=" + email + "&select=" + select + "&terms=" + terms, 
+        //     success: function(text) {
+        //         if (text == "success") {
+        //             gformSuccess();
+        //         } else {
+        //             gformError();
+        //             gsubmitMSG(false, text);
+        //         }
+        //     }
+        // });
+	}
+
+    function gformSuccess() {
+        $("#getEbookForm")[0].reset();
+        gsubmitMSG(true, "Message Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function gformError() {
+        $("#getEbookForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function gsubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#gmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+	}
+
+    
+	/* Contact Form */
+    $("#contactForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            cformError();
+            csubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            csubmitForm();
+        }
+    });
+
+
+
+    function cformError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function csubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+
+    /* Privacy Form */
+    $("#privacyForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            pformError();
+            psubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            psubmitForm();
+        }
+    });
+
+    function psubmitForm() {
+        // initiate variables with form content
+		var name = $("#pname").val();
+		var email = $("#pemail").val();
+        var select = $("#pselect").val();
+        var terms = $("#pterms").val();
+        
+        $.ajax({
+            type: "POST",
+            url: "php/privacy-process.php",
+            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms, 
+            success: function(text) {
+                if (text == "success") {
+                    pformSuccess();
+                } else {
+                    pformError();
+                    psubmitMSG(false, text);
+                }
+            }
+        });
+	}
+
+    function pformSuccess() {
+        $("#privacyForm")[0].reset();
+        psubmitMSG(true, "Request Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function pformError() {
+        $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function psubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+    
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
